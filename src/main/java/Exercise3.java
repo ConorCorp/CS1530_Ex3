c6309945ad8c36360a386c3cf057a0d489dd035f
Gideon Shils <gideon.shils@gmail.com>
2017-10-25 00:15:13 -0400

    Finish lazy and add tests

--- src/main/java/Exercise3.java
+++ src/main/java/Exercise3.java
@@ -1,20 +1,39 @@
 public class Exercise3 {
 
   public static void main(String args[]) {
-    if( args.length != 1 ) {
-      System.out.println("Incorrect # arguments: Exiting....");
+
+    // Check if correct number of arguments
+    if ( args.length != 1 ) {
+      System.out.println("Incorrect number of arguments: Exiting...");
       System.exit(1);
     }
 
-    int n = Integer.parseInt(args[0]);
+    // Calculate nth numbers in sequences only if argument is an integer
+    try {
+  
+      int n = Integer.parseInt(args[0]);
 
-    System.out.println("Tri("+n+") = "+Tri(n));
+      // Calculate and print the nth numbers in the sequences
+      System.out.println("Tri(" + n + ") = " + Tri(n));
+      System.out.println("Lazy(" + n + ") = " + Lazy(n));
+
+    } catch (NumberFormatException e) {
+      System.out.println("Argument is not an integer: Exiting...");
+      System.exit(2);
+    }
 
 
   }
 
-    public static int Tri(int n) {
-      return (n * (n + 1)) / 2;
-    }
+  // Calculate nth value in triangle sequence
+  public static int Tri(int n) {
+    return (n * (n + 1)) / 2;
+  }
+
+  // Calculate nth value in lazy canter sequence
+  public static int Lazy(int n) {
+    return ((n*n) + n + 2)/2;
+  }
+
 
 }
--- src/test/java/Exercise3Tests.java
+++ src/test/java/Exercise3Tests.java
@@ -3,6 +3,7 @@ import static org.junit.Assert.*;
 
 public class Exercise3Tests {
 
+  // Tests for Triangle sequence
   @Test
   public void testTri() {
     assertEquals(Exercise3.Tri(0), 0);
@@ -16,6 +17,22 @@ public class Exercise3Tests {
   @Test
   public void testTri7() {
     assertEquals(Exercise3.Tri(7), 28);
+  }
+
+  // Tests for Lazy Canter sequence
+  @Test
+  public void testLazy() {
+    assertEquals(Exercise3.Lazy(0), 1);
+  }
+
+  @Test
+  public void testLazy2() {
+    assertEquals(Exercise3.Lazy(2), 4);
+  }
+
+  @Test
+  public void testLazy7() {
+    assertEquals(Exercise3.Lazy(7), 29);
   }
 
 }
